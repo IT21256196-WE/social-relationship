4c79cfc22d8c964b6339982222249b27d91ea2b9
RxshiA <rashiamarasiri@gmail.com>
2025-05-25 19:11:21 +0530

    feat: adding the new learning path

--- ml/train_difficulty.py
+++ ml/train_difficulty.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 
 # Data preparation
 X = np.array([
-    [0.8, 0.9, 1], 
-    [0.4, 0.5, 2], 
+    [0.8, 0.9, 1],
+    [0.4, 0.5, 2],
     [0.6, 0.7, 1]
 ])
 y = np.array([1, 3, 2])
@@ -15,6 +15,8 @@ y = np.array([1, 3, 2])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Model
+
+
 class DifficultyRegressor(nn.Module):
     def __init__(self):
         super(DifficultyRegressor, self).__init__()
@@ -29,6 +31,7 @@ class DifficultyRegressor(nn.Module):
         x = self.fc3(x)
         return x
 
+
 model = DifficultyRegressor()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
@@ -38,10 +41,10 @@ for epoch in range(500):
     model.train()
     inputs = torch.tensor(X_train, dtype=torch.float32)
     targets = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
-    
+
     outputs = model(inputs)
     loss = criterion(outputs, targets)
-    
+
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
